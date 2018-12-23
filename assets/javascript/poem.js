5c61216582b401e3a4e31fcf42758fce45d07034
Theo <theobasallaje@gmail.com>
2018-12-22 21:51:15 -0600

    folder structure

---
+++ assets/javascript/poem.js
@@ -0,0 +1,7 @@
+var poemArray = [
+    "My Poem, My Website My poem, my website My story, my birthright It’s murder under moonlight He put up a good fight It’s over, call CareFlight My alibi’s airtight My victim could be right But he’s dead, so I’m right"
+];
+
+var poem = document.getElementById("poem");
+
+poem.textContent("poem: " + poemArray[0]);
