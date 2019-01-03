f1aef164bcbfad638e40178526e4e146b5e4eeed
Theo <theobasallaje@gmail.com>
2019-01-02 20:01:31 -0600

    widened poem cards and click event for poem view

--- assets/javascript/poem.js
+++ assets/javascript/poem.js
@@ -1,13 +1,35 @@
+//onjects 
+var poem = {
+    
+}
+//arrays
 var poemArray = [
     "My Poem, My Website My poem, my website My story, my birthright It’s murder under moonlight He put up a good fight It’s over, call CareFlight My alibi’s airtight My victim could be right But he’s dead, so I’m right"
 ];
 
-// var poem = document.getElementById("poem");
-// var poem2 = document.getElementsByClassName("poem2");
-var poem = document.querySelectorAll(".poemText");
+//global variables
+var poemText = document.querySelectorAll(".poemText");
 var title = document.querySelectorAll(".title");
-// poem.textContent = poemArray[0];
-for (i = 0; i < poem.length; i++) {
-    title[i].textContent = "Title";
-    poem[i].textContent = poemArray[0];
+var poemCard = $(".poem");
+var poemView = $("#poemView");
+
+//function calls
+displayPoems();
+
+//functions 
+function displayPoems(){
+    for (i = 0; i < poemText.length; i++) {
+        title[i].textContent = "Title";
+        poemText[i].textContent = poemArray[0];
+    }
 }
+
+poemCard.click(function(){
+    poemView.html(`<div class="poem">
+                   <h1 class="title"></h1>
+                   <div class="poemText"></div>
+                   </div>`);
+});
+
+
+
